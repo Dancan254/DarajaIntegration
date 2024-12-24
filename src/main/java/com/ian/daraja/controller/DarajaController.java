3605b58dab695305a7bac567769e1a659bc34382
Dancan254 <dancanian25@gmail.com>
2024-12-24 10:12:26 +0300

    Add validation endpoint to DarajaController for Mpesa validation requests

--- src/main/java/com/ian/daraja/controller/DarajaController.java
+++ src/main/java/com/ian/daraja/controller/DarajaController.java
@@ -1,25 +1,26 @@
 package com.ian.daraja.controller;
 
 import com.ian.daraja.dto.AccessTokenResponse;
+import com.ian.daraja.dto.AcknowledgementResponse;
+import com.ian.daraja.dto.MpesaValidationResponse;
 import com.ian.daraja.dto.RegisterUrlResponse;
 import com.ian.daraja.services.DarajaAPI;
 import com.ian.daraja.services.DarajaApiImpl;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.io.IOException;
 
+@Slf4j
 @RestController
 @RequestMapping("/api/v1/daraja")
 @RequiredArgsConstructor
 public class DarajaController {
     private final DarajaAPI darajaAPI;
-
+    private final AcknowledgementResponse acknowledgementResponse;
     @GetMapping(value = "/access-token", produces = "application/json")
     public ResponseEntity<AccessTokenResponse> getAccessToken() {
         try{
@@ -33,6 +34,16 @@ public class DarajaController {
     public ResponseEntity<RegisterUrlResponse> registerUrl() {
         try{
             return ResponseEntity.ok(darajaAPI.registerUrl());
+        } catch (Exception e) {
+            return ResponseEntity.badRequest().build();
+        }
+    }
+
+    @PostMapping(value = "/validation", produces = "application/json")
+    public ResponseEntity<AcknowledgementResponse> validation(@RequestBody MpesaValidationResponse mpesaValidationResponse) {
+        try{
+            log.info("Received validation request: {}", mpesaValidationResponse);
+            return ResponseEntity.ok(acknowledgementResponse);
         } catch (Exception e) {
             return ResponseEntity.badRequest().build();
         }
