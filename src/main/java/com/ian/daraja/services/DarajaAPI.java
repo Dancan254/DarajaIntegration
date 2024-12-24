998705a6da97235ac048e0bcb9a2f0d22f50ee9d
Dancan254 <dancanian25@gmail.com>
2024-12-24 14:41:32 +0300

    Add simulateC2BTransaction method to DarajaAPI for processing C2B transaction requests

--- src/main/java/com/ian/daraja/services/DarajaAPI.java
+++ src/main/java/com/ian/daraja/services/DarajaAPI.java
@@ -1,6 +1,8 @@
 package com.ian.daraja.services;
 
 import com.ian.daraja.dto.AccessTokenResponse;
+import com.ian.daraja.dto.C2BTransactionRequest;
+import com.ian.daraja.dto.C2BTransactionResponse;
 import com.ian.daraja.dto.RegisterUrlResponse;
 
 import java.io.IOException;
@@ -9,4 +11,5 @@ public interface DarajaAPI {
     AccessTokenResponse getAccessToken();
 
     RegisterUrlResponse registerUrl();
+    C2BTransactionResponse simulateC2BTransaction(C2BTransactionRequest c2BTransactionRequest) throws IOException;
 }
