737d8b0797cd411fbbe505345f0996b22db6ea80
Dancan254 <dancanian25@gmail.com>
2024-12-26 15:29:27 +0300

    Add b2cPaymentRequest method to DarajaAPI for processing B2C payment requests

--- src/main/java/com/ian/daraja/services/DarajaAPI.java
+++ src/main/java/com/ian/daraja/services/DarajaAPI.java
@@ -1,9 +1,6 @@
 package com.ian.daraja.services;
 
-import com.ian.daraja.dto.AccessTokenResponse;
-import com.ian.daraja.dto.C2BTransactionRequest;
-import com.ian.daraja.dto.C2BTransactionResponse;
-import com.ian.daraja.dto.RegisterUrlResponse;
+import com.ian.daraja.dto.*;
 
 import java.io.IOException;
 
@@ -12,4 +9,5 @@ public interface DarajaAPI {
 
     RegisterUrlResponse registerUrl();
     C2BTransactionResponse simulateC2BTransaction(C2BTransactionRequest c2BTransactionRequest) throws IOException;
+    B2CResponse b2cPaymentRequest(B2CRequest b2CRequest) throws IOException;
 }
