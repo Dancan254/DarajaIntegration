c035050a65e819532232f7b7d1d4f9d6483f40f9
Dancan254 <dancanian25@gmail.com>
2024-12-26 16:51:47 +0300

    Add StkPushResponse DTO for handling STK push responses

---
+++ src/main/java/com/ian/daraja/dto/StkPushResponse.java
@@ -0,0 +1,18 @@
+package com.ian.daraja.dto;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+import lombok.Data;
+
+@Data
+public class StkPushResponse {
+    @JsonProperty("MerchantRequestID")
+    private String MerchantRequestID;
+    @JsonProperty("CheckoutRequestID")
+    private String CheckoutRequestID;
+    @JsonProperty("ResponseCode")
+    private String ResponseCode;
+    @JsonProperty("ResponseDescription")
+    private String ResponseDescription;
+    @JsonProperty("CustomerMessage")
+    private String CustomerMessage;
+}
