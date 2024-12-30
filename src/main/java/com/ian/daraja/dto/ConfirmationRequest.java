b6b62a9a46a34cb23ff5e692972331da02658b68
Dancan254 <dancanian25@gmail.com>
2024-12-30 10:21:42 +0300

    Add ConfirmationRequest DTO for handling confirmation requests

---
+++ src/main/java/com/ian/daraja/dto/ConfirmationRequest.java
@@ -0,0 +1,46 @@
+package com.ian.daraja.dto;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+import lombok.Data;
+
+@Data
+public class ConfirmationRequest {
+    @JsonProperty("TransactionType")
+    private String transactionType;
+
+    @JsonProperty("TransID")
+    private String transID;
+
+    @JsonProperty("TransTime")
+    private String transTime;
+
+    @JsonProperty("TransAmount")
+    private String transAmount;
+
+    @JsonProperty("BusinessShortCode")
+    private String businessShortCode;
+
+    @JsonProperty("BillRefNumber")
+    private String billRefNumber;
+
+    @JsonProperty("InvoiceNumber")
+    private String invoiceNumber;
+
+    @JsonProperty("OrgAccountBalance")
+    private String orgAccountBalance;
+
+    @JsonProperty("ThirdPartyTransID")
+    private String thirdPartyTransID;
+
+    @JsonProperty("MSISDN")
+    private String msisdn;
+
+    @JsonProperty("FirstName")
+    private String firstName;
+
+    @JsonProperty("MiddleName")
+    private String middleName;
+
+    @JsonProperty("LastName")
+    private String lastName;
+}
